f1ad5613b53e3b8682bf5a08ef0f8d1e6a63de96
Crystalcxd <547339930@qq.com>
2016-09-17 20:20:21 +0800

    Initial Commit

---
+++ MotherSleep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MotherSleep
+//
+//  Created by Michael on 2016/9/17.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MotherSleep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MotherSleep
+//
+//  Created by Michael on 2016/9/17.
+//  Copyright © 2016年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
